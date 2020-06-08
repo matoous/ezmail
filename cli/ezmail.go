962a10c6830c5ab7e10944b2e665237d66c220c1
Matous Dzivjak <matousdzivjak@gmail.com>
2020-06-08 14:58:01 +0200

    feat: Add support for CC, BCC, html in email

--- cli/ezmail.go
+++ cli/ezmail.go
@@ -1,108 +1,124 @@
 package cli
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/smtp"
-    "os"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"net/smtp"
+	"os"
+	"strings"
 
-    "github.com/spf13/cobra"
+	"github.com/jaytaylor/html2text"
+	"github.com/jordan-wright/email"
+	"github.com/spf13/cobra"
 
-    "github.com/matoous/ezmail/cli/accounts"
-    "github.com/matoous/ezmail/internal"
+	"github.com/matoous/ezmail/cli/accounts"
+	"github.com/matoous/ezmail/internal"
 )
 
 type CLI struct {
-    RootCmd *cobra.Command
-    Config  *internal.Config
+	RootCmd *cobra.Command
+	Config  *internal.Config
 }
 
 func rootCmd(cfg *internal.Config) *cobra.Command {
-    var subject string
-    var account string
-    //var cc []string
-    //var bcc []string
+	var subject string
+	var account string
+	var html bool
+	var cc []string
+	var bcc []string
 
-    cmd := &cobra.Command{
-        Use:   "ezmail",
-        Short: "Send email from terminal with eas",
-        Args:  cobra.MinimumNArgs(1),
-        Run: func(cmd *cobra.Command, args []string) {
-            if len(cfg.Accounts) == 0 {
-                cmd.PrintErr("no accounts")
-                return
-            }
+	cmd := &cobra.Command{
+		Use:   "ezmail",
+		Short: "Send email from terminal with eas",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(cfg.Accounts) == 0 {
+				cmd.PrintErr("no accounts")
+				return
+			}
 
-            var ac *internal.Account
-            if account == "" {
-                ac = cfg.Accounts.Default()
-            } else {
-                ac = cfg.Accounts.ByID(account)
-            }
+			var ac *internal.Account
+			if account == "" {
+				ac = cfg.Accounts.Default()
+			} else {
+				ac = cfg.Accounts.ByID(account)
+			}
 
-            // get password for the selected user from OS keychain
-            pw, err := ac.Password()
-            if err != nil {
-                cmd.PrintErr(err)
-                return
-            }
+			// get password for the selected user from OS keychain
+			pw, err := ac.Password()
+			if err != nil {
+				cmd.PrintErr(err)
+				return
+			}
 
-            from := ac.Username
-            to := args
+			from := ac.Username
+			to := args
 
-            in, err := ioutil.ReadAll(os.Stdin)
-            if err != nil {
-                cmd.PrintErr(err)
-                return
-            }
-            message := []byte(fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\n\n%s", from, strings.Join(to, ", "), subject, in))
+			in, err := ioutil.ReadAll(os.Stdin)
+			if err != nil {
+				cmd.PrintErr(err)
+				return
+			}
 
-            auth := smtp.PlainAuth("", ac.Username, string(pw), strings.Split(ac.SMTPServer, ":")[0])
-            err = smtp.SendMail(ac.SMTPServer, auth, from, to, message)
-            if err != nil {
-                cmd.PrintErrf("sending email: %s", err)
-                return
-            }
-            fmt.Println("📧 email successfully sent")
-        },
-    }
+			e := email.NewEmail()
+			e.From = from
+			e.To = to
+			e.Subject = subject
+			e.Text = in
+			if html {
+				text, _ := html2text.FromString(string(in), html2text.Options{PrettyTables: true})
+				e.Text = []byte(text)
+				e.HTML = in
+			}
 
-    cmd.Flags().StringVarP(&subject, "subject", "s", "", "email subject")
-    cmd.Flags().StringVarP(&account, "account", "a","", "account to use for sending, leave empty for default account")
+			auth := smtp.PlainAuth("", ac.Username, string(pw), strings.Split(ac.SMTPServer, ":")[0])
+			err = e.Send(ac.SMTPServer, auth)
+			if err != nil {
+				cmd.PrintErrf("sending email: %s", err)
+				return
+			}
+			fmt.Println("📧 email successfully sent")
+		},
+	}
 
-    return cmd
+	cmd.Flags().StringVarP(&subject, "subject", "s", "", "email subject")
+	cmd.Flags().StringVarP(&account, "account", "a", "", "account to use for sending, leave empty for default account")
+	cmd.Flags().StringSliceVarP(&cc, "CC", "c", []string{}, "recipients in CC")
+	cmd.Flags().StringSliceVarP(&bcc, "BCC", "b", []string{}, "recipients in BCC")
+	cmd.Flags().BoolVar(&html, "html", false, "marks the input as html, ezmail will also try to convert it to text")
+
+	return cmd
 }
 
 func New() (*CLI, error) {
-    cfg, err := internal.LoadConfig()
-    if err != nil {
-        panic(err)
-    }
+	cfg, err := internal.LoadConfig()
+	if err != nil {
+		panic(err)
+	}
 
-    cli := &CLI{
-        Config: cfg,
-        RootCmd: rootCmd(cfg),
-    }
+	cli := &CLI{
+		Config:  cfg,
+		RootCmd: rootCmd(cfg),
+	}
 
-    cli.RootCmd.AddCommand(&cobra.Command{
-        Use: "version",
-        Short: "Print the version number of ezmail",
-        Run: func(cmd *cobra.Command, args []string) {
-            fmt.Printf("Ezmail mail sending utility %s (%s)\n", internal.BuildRev, internal.CommitDate)
-        },
-    })
+	cli.RootCmd.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of ezmail",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Ezmail mail sending utility %s (%s)\n", internal.BuildRev, internal.CommitDate)
+		},
+	})
 
-    cli.RootCmd.AddCommand(
-        accounts.Command(cfg),
-    )
+	cli.RootCmd.AddCommand(
+		accounts.Command(cfg),
+	)
 
-    return cli, nil
+	return cli, nil
 }
 
 func (c *CLI) Execute() {
-    if err := c.RootCmd.Execute(); err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err := c.RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,14 @@ go 1.14
 
 require (
 	github.com/go-openapi/strfmt v0.19.5 // indirect
+	github.com/jaytaylor/html2text v0.0.0-20200412013138-3577fbdbcff7
 	github.com/jedib0t/go-pretty v4.3.0+incompatible
+	github.com/jordan-wright/email v0.0.0-20200602115436-fd8a7622303e
 	github.com/keybase/go-keychain v0.0.0-20200502122510-cda31fe0c86d
 	github.com/matoous/go-nanoid v1.4.1
 	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v0.0.4 // indirect
 	github.com/spf13/cobra v1.0.0
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
 	golang.org/x/crypto v0.0.0-20200323165209-0ec3e9974c59
 )
